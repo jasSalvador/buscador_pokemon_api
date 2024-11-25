3ce65a1132d07e813f3e602290e7e735375d8ce7
Jasmin Salvador <sjasmin.salvador@gmail.com>
2024-11-25 17:27:44 -0300

    iniciando repositorio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="bg-danger pb-5">
+      <h1 class="text-center mx-auto pt-5 mb-3 text-warning fw-bold">Buscador de Pokémon</h1>
+
+      <div class="container d-flex flex-wrap justify-content-between align-items-center mx-auto">
+        <div class="col-lg-5 col-md-7 col-sm-10 border border-secondary mt-5 bg-light">
+          <div class="bg-secondary bg-opacity-50">
+            <p class="ps-3 py-1">Buscador</p>
+          </div>
+          <div class="d-flex justify-content-center align-items-center mx-auto gap-3 p-3">
+            <input type="text" v-model="searchPokemon" class="form-control" placeholder="Ingresa ID o Nombre">
+            <button @click="searchPokemonName" class="btn btn-success">Buscar</button>
+          </div>
+        </div>
+
+        <div class="col-lg-5 col-md-8 col-sm-10 border border-secondary mt-5 bg-light">
+          <div class="bg-secondary bg-opacity-50">
+            <p class="ps-3 py-1">Pokémon</p>
+          </div>
+          <div class="px-3">           
+            <p class=""><span class="fw-bold">ID:</span> {{pokemon.id}}</p>
+            <p class=""><span class="fw-bold">Nombre:</span> {{pokemon.name}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr class="my-0 linea">
+    
+    <div class="text-center mx-auto mb-5 col-2">
+      <img :src="pokemon.sprite" alt="Pokemon" v-if="pokemon.sprite" class="w-100 d-block">
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
 
-export default {
+export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      searchPokemon: '',
+      pokemon: {
+        id: '',
+        name: '',
+        sprite: ''
+      }
+    }
+  },
+methods: {
+    async searchPokemonName() {
+      if (!this.searchPokemon) {
+        alert('Debes ingresar un ID o Nombre');
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.searchPokemon.toLowerCase()}`
+        );
+
+        const data = await response.json();
+
+        //actualizar la info
+        this.pokemon = {
+          id: data.id,
+          name: data.name,
+          sprite: data.sprites.front_default
+        };
+      } catch (error) {
+        console.error(error);
+        alert('No se encontró el Pokémon');
+        //limpiar
+        this.pokemon = { id: '', name: '', sprite: '' };
+      }
+    }
+  },
+  created() {
+    //pokémon por defecto
+    this.searchPokemon = 'pikachu';
+    this.searchPokemonName();
   }
-}
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .linea {
+    border: 8px solid #07000a;
+    opacity: 8;
+  }
 </style>
